c7e1f86bb1a3dac55b2631613d38bccea50c43a7
HshMaker <man12297565@gmail.com>
2024-08-11 00:27:46 +0900

    cleanup

--- setup.py
+++ setup.py
@@ -12,4 +12,11 @@ setup(
     author='HshMaker',
     author_email='hshmakerss@gmail.com',
     description="그냥 히읗이 많이 들어간 언어입니다.",
+    long_description=open("../README.md", "r", encoding="UTF-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/HshMaker/hamlang",
+    classifiers=[
+        "Intended Audience :: Friends",
+        "License :: OSI Approved :: MIT License"
+    ]
 )
